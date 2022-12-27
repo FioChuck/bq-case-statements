2011e402975d6e8f6c9eb05f0b73c9a9d734a0bd
FiocChuck <chasf@google.com>
2022-12-27 09:30:45 -0500

    define classes

--- src/main.py
+++ src/main.py
@@ -9,6 +9,77 @@ import time
 start_time = time.time()
 
 
+class Record:
+
+    def __init__(self):
+        self.fake = Faker()
+        self.name_list = [
+            "Ria",
+            "Gary",
+            "Miah",
+            "Gabriela",
+            "Musa",
+            "Jaime",
+            "Solomon",
+            "Chelsea",
+            "Connor",
+            "Alessia"
+        ]
+
+    def fake_values(self):
+        self.name = random.choice(self.name_list)
+        self.id = str(uuid.uuid1())
+        self.archive_date = self.fake.date_between(
+            start_date='-6M', end_date='-3M')
+        self.dispute_date = self.fake.date_between(
+            start_date='-3M', end_date='now')
+
+        # return name, id, archive_date, dispute_date
+
+
+class Record_list:
+
+    def __init__(self):
+        self.disputes_records = []
+        self.archives_records = []
+
+    def fake_array(self, fake):
+
+        self.disputes_records.insert(0, {
+            "id": id,
+            "name": fake.name,
+            "dispute_date": fake.dispute_date
+        })
+
+        self.archives_records.insert(0, {
+            "id": id,
+            "archive_date": fake.archive_date
+        })
+
+        self.disputes_df = pandas.DataFrame(
+            self.disputes_records,
+            columns=[
+                "id",
+                "name",
+                "dispute_date"
+            ])
+
+        self.archives_df = pandas.DataFrame(
+            self.archives_records,
+            columns=[
+                "id",
+                "archive_date"
+            ]
+        )
+
+
+fake_record = Record()  # instantiate object
+fake_record.fake_values()  # set fake values
+
+fake_records = Record_list()
+fake_records.fake_array(fake_record)
+
+
 def main(request):
     client = bigquery.Client()
 
