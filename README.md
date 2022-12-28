99001ae8d547c09a90b9fec9aee5a55d2b16bcbc
FiocChuck <chasf@google.com>
2022-12-28 09:58:00 -0500

    update md

--- README.md
+++ README.md
@@ -1,1 +1,137 @@
-# bq-case-statements
+# TL;DR
+
+A simple Python GCP Cloud Function to insert mock transaction data into BigQuery.
+
+The mock data and BigQuery statements _(below)_ are used to highlight a SQL anti-pattern and recommend alternatives.
+
+The two SQL queries shown below achieve the same result; however, one technique is more optimized. This example was created to document the importance of columnar transformations versus multiple left joins.
+
+# Optimized Query
+
+```sql
+SELECT
+  t0.id,
+  t0.name,
+  t0.dispute_date,
+  t1.archive_date,
+  CASE
+    WHEN DATE_DIFF(t1.archive_date, t0.dispute_date,month) = -1 THEN t1.archive_date
+END
+  AS archive_date1,
+  CASE
+    WHEN DATE_DIFF(t1.archive_date, t0.dispute_date,month) = -2 THEN t1.archive_date
+END
+  AS archive_date2,
+  CASE
+    WHEN DATE_DIFF(t1.archive_date, t0.dispute_date,month) = -3 THEN t1.archive_date
+END
+  AS archive_date3,
+  CASE
+    WHEN DATE_DIFF(t1.archive_date, t0.dispute_date,month) = -4 THEN t1.archive_date
+END
+  AS archive_date4,
+  CASE
+    WHEN DATE_DIFF(t1.archive_date, t0.dispute_date,month) = -5 THEN t1.archive_date
+END
+  AS archive_date5,
+  CASE
+    WHEN DATE_DIFF(t1.archive_date, t0.dispute_date,month) = -6 THEN t1.archive_date
+END
+  AS archive_date6
+FROM
+  case_statement.disputes AS t0
+LEFT JOIN
+  `cf-data-analytics.case_statement.archives` AS t1
+ON
+  t0.id = t1.id
+```
+
+# Inefficient Left Join
+
+```sql
+SELECT
+  t0.*,
+  t1.archive_date AS archive_date1,
+  t2.archive_date AS archive_date2,
+  t3.archive_date AS archive_date3,
+  t4.archive_date AS archive_date4,
+  t5.archive_date AS archive_date5,
+  t6.archive_date AS archive_date6
+FROM
+  case_statement.disputes AS t0
+LEFT JOIN
+  `cf-data-analytics.case_statement.archives` AS t1
+ON
+  t0.id = t1.id
+  AND DATE_DIFF(t1.archive_date, t0.dispute_date,month) = -1
+LEFT JOIN
+  `cf-data-analytics.case_statement.archives` AS t2
+ON
+  t0.id = t2.id
+  AND DATE_DIFF(t2.archive_date, t0.dispute_date,month) = -2
+LEFT JOIN
+  `cf-data-analytics.case_statement.archives` AS t3
+ON
+  t0.id = t3.id
+  AND DATE_DIFF(t3.archive_date, t0.dispute_date,month) = -3
+LEFT JOIN
+  `cf-data-analytics.case_statement.archives` AS t4
+ON
+  t0.id = t4.id
+  AND DATE_DIFF(t4.archive_date, t0.dispute_date,month) = -4
+  LEFT JOIN
+  `cf-data-analytics.case_statement.archives` AS t5
+ON
+  t0.id = t5.id
+  AND DATE_DIFF(t5.archive_date, t0.dispute_date,month) = -5
+  LEFT JOIN
+  `cf-data-analytics.case_statement.archives` AS t6
+ON
+  t0.id = t6.id
+  AND DATE_DIFF(t6.archive_date, t0.dispute_date,month) = -6
+```
+
+# Setup
+
+This project includes a yaml file for deployment to Google Cloud using Github Actions maintained here: https://github.com/google-github-actions/deploy-cloud-functions. The Github Action Workflow requires several _"Action Secrets"_ used to set environment variables during deployment. Set the following secrets in the repository before deployment.
+
+| Action Secret  | Value                                                          |
+| -------------- | -------------------------------------------------------------- |
+| GCP_PROJECT_ID | GCP Project ID where Function will be deployed                 |
+| GCP_SA_KEY     | Service Account Key used to authenticate GitHub to GCP Project |
+
+Two tables are also required in BigQuery to run this example.
+
+## `archives` table
+
+```json
+[
+  {
+    "name": "id",
+    "type": "STRING"
+  },
+  {
+    "name": "archive_date",
+    "type": "DATE"
+  }
+]
+```
+
+## `disputes` table
+
+```json
+[
+  {
+    "name": "id",
+    "type": "STRING"
+  },
+  {
+    "name": "name",
+    "type": "STRING"
+  },
+  {
+    "name": "dispute_date",
+    "type": "DATE"
+  }
+]
+```
--- src/main.py
+++ src/main.py
@@ -27,14 +27,14 @@ class Record:
         ]
 
     def fake_values(self):
+        # set name to random value from list
         self.name = random.choice(self.name_list)
-        self.id = str(uuid.uuid1())
+
+        self.id = str(uuid.uuid1())  # set id to uuid value
         self.archive_date = self.fake.date_between(
             start_date='-6M', end_date='-3M')
         self.dispute_date = self.fake.date_between(
             start_date='-3M', end_date='now')
-
-        # return name, id, archive_date, dispute_date
 
 
 class Record_list:
@@ -43,19 +43,20 @@ class Record_list:
         self.disputes_records = []
         self.archives_records = []
 
-    def fake_array(self, fake):
+    def fake_list(self, fake):
 
         self.disputes_records.insert(0, {
-            "id": id,
+            "id": fake.id,
             "name": fake.name,
             "dispute_date": fake.dispute_date
         })
 
         self.archives_records.insert(0, {
-            "id": id,
+            "id": fake.id,
             "archive_date": fake.archive_date
         })
 
+    def fake_df(self):
         self.disputes_df = pandas.DataFrame(
             self.disputes_records,
             columns=[
@@ -73,124 +74,49 @@ class Record_list:
         )
 
 
-fake_record = Record()  # instantiate object
-fake_record.fake_values()  # set fake values
+def bq_write(df, table, schema):
 
-fake_records = Record_list()
-fake_records.fake_array(fake_record)
-
-
-def main(request):
     client = bigquery.Client()
 
-    print('Creating Records')
-
-    # reset records to insert
-    disputes_records = []
-    archives_records = []
-
-    for x in range(5000):
-        id = str(uuid.uuid1())
-
-        fake = Faker()  # fake proxy object
-        archive_date = fake.date_between(start_date='-6M', end_date='-3M')
-        dispute_date = fake.date_between(start_date='-3M', end_date='now')
-
-        # initializing name list
-        name_list = [
-            "Ria",
-            "Gary",
-            "Miah",
-            "Gabriela",
-            "Musa",
-            "Jaime",
-            "Solomon",
-            "Chelsea",
-            "Connor",
-            "Alessia"
-        ]
-
-        random_name = random.choice(name_list)
-
-        disputes_records.insert(0, {
-            "id": id,
-            "name": random_name,
-            "dispute_date": dispute_date
-        })
-
-        archives_records.insert(0, {
-            "id": id,
-            "archive_date": archive_date
-        })
-
-    disputes_df = pandas.DataFrame(
-        disputes_records,
-        columns=[
-            "id",
-            "name",
-            "dispute_date"
-        ])
-
-    archives_df = pandas.DataFrame(
-        archives_records,
-        columns=[
-            "id",
-            "archive_date"
-        ]
-    )
-    print('Done Creating Records')
-    print("--- %s seconds ---" % (time.time() - start_time))
-
-    ######################
-    #### Write to BQ #####
-    ######################
-    print('Writing Archives to BigQuery')
-
     job_config = bigquery.LoadJobConfig(
-        # Specify a (partial) schema. All columns are always written to the
-        # table. The schema is used to assist in data type definitions.
-        schema=[
-            bigquery.SchemaField("id", bigquery.enums.SqlTypeNames.STRING),
-            bigquery.SchemaField(
-                "archive_date", bigquery.enums.SqlTypeNames.DATE),
-        ],
-        write_disposition="WRITE_APPEND",
+        schema=schema,
+        write_disposition="WRITE_APPEND",  # append values into bigquey table
     )
 
-    table_id = "cf-data-analytics.case_statement.archives"
     job = client.load_table_from_dataframe(
-        archives_df, table_id, job_config=job_config
+        df, table, job_config=job_config
     )  # Make an API request.
+
     job.result()  # Wait for the job to complete.
 
-    print('Done Writing Archives to BigQuery')
-    print("--- %s seconds ---" % (time.time() - start_time))
 
-    ######################
-    #### Write to BQ #####
-    ######################
-    print('Writing Disputes to BigQuery')
+######### CALCULATIONS ###################
 
-    job_config = bigquery.LoadJobConfig(
-        # Specify a (partial) schema. All columns are always written to the
-        # table. The schema is used to assist in data type definitions.
-        schema=[
-            bigquery.SchemaField("id", bigquery.enums.SqlTypeNames.STRING),
-            bigquery.SchemaField(
-                "name", bigquery.enums.SqlTypeNames.STRING),
-            bigquery.SchemaField(
-                "dispute_date", bigquery.enums.SqlTypeNames.DATE),
-        ],
-        write_disposition="WRITE_APPEND",
-    )
+archive_schema = [
+    bigquery.SchemaField("id", bigquery.enums.SqlTypeNames.STRING),
+    bigquery.SchemaField("archive_date", bigquery.enums.SqlTypeNames.DATE)
+]
 
-    table_id = "cf-data-analytics.case_statement.disputes"
-    job = client.load_table_from_dataframe(
-        disputes_df, table_id, job_config=job_config
-    )  # Make an API request.
-    job.result()  # Wait for the job to complete.
+disputes_schema = [
+    bigquery.SchemaField("id", bigquery.enums.SqlTypeNames.STRING),
+    bigquery.SchemaField("name", bigquery.enums.SqlTypeNames.STRING),
+    bigquery.SchemaField("dispute_date", bigquery.enums.SqlTypeNames.DATE),
+]
 
-    print('Done Writing Disputes to BigQuery')
-    print("--- %s seconds ---" % (time.time() - start_time))  # done
 
-    return 'finish'
+fake_record = Record()  # instantiate Record object
+fake_record_array = Record_list()  # instatiate Record_list object
+
+for x in range(1000000):
+    fake_record.fake_values()  # set fake values
+    fake_record_array.fake_list(fake_record)  # insert values into list
+
+fake_record_array.fake_df()  # insert list into pandas dataframe
+
+print('Done Creating Dataframes')
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# 100,000 in 10.0 seconds
+
+# bq_write(fake_record_array.archives_df,
+#          "cf-data-analytics.case_statement.archives", archive_schema)
